b3592071b25ab49b69dbc77f3d021bf2b38295f3
Björn Martensen <bjoern.martensen@gmail.com>
2014-09-22 14:45:35 +0200

    fix

--- wp-install.sh
+++ wp-install.sh
@@ -5,7 +5,7 @@
 # ------------ CONFIGURE THIS ------------
 SITE_NAME=
 THEME_NAME=wp-starter-theme
-DB_NAME=wp_starter_v2
+DB_NAME=
 
 MY_IP=192.168.1.99
 MY_PORT=8080
@@ -42,7 +42,6 @@ EOF
 }
 
 
-
 # ------------ Parameters ------------
 while getopts "h:s:t:d:" OPTION
 do
@@ -74,142 +73,163 @@ then
 fi
 
 
-# ------------ Set up the database ------------
-echo "--- Setting up database ---"
-mysql --user=$DB_USER --password=$DB_PASSWORD --host=$DB_HOST --port=$DB_PORT << QUERY_INPUT
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
+create_database()
+{
+  echo "--- Setting up database ---"
+  mysql --user=$DB_USER --password=$DB_PASSWORD --host=$DB_HOST --port=$DB_PORT << QUERY_INPUT
+  CREATE DATABASE IF NOT EXISTS $DB_NAME;
 QUERY_INPUT
+}
 
+function configure_wp_cli {
+  mkdir $SITE_NAME
+  cd $SITE_NAME
 
+  cat << EOF > wp-cli.local.yml
+  path: public/wordpress
 
-# ------------ WordPress installation ------------
-# echo "--- Cloning repository---"
-# git clone --recursive git@github.com:baze/wp-starter.git $SITE_NAME
+  core install:
+    title: WordPress Starter
+    url: http://wp-starter.dev
+    admin_user: starter-admin
+    admin_password: starter-password
+    admin_email: starter-admin@euw.de
 
-mkdir $SITE_NAME
-cd $SITE_NAME
+  core config:
+    dbname: $DB_NAME
+    dbuser: $DB_USER
+    dbpass: $DB_PASSWORD
+    dbhost: $DB_HOST:$DB_PORT
 
-cat << EOF > wp-cli.local.yml
-path: public/wordpress
+  apache_modules:
+    - mod_rewrite
 
-core install:
-  title: WordPress Starter
-  url: http://wp-starter.dev
-  admin_user: starter-admin
-  admin_password: starter-password
-  admin_email: starter-admin@euw.de
+  ssh:
+    vagrant:
+      # The %pseudotty% placeholder gets replaced with -t or -T depending on whether you're piping output
+      # The %cmd% placeholder is replaced with the originally-invoked WP-CLI command
+      cmd: vagrant ssh-config > /tmp/vagrant_ssh_config && ssh -q %pseudotty% -F /tmp/vagrant_ssh_config default %cmd%
+      # Passed to WP-CLI on the remote server via --url
+      url: 192.168.33.20
+      # We cd to this path on the remote server before running WP-CLI
+      path: /vagrant/sites/wp-starter/public/wordpress/
+  EOF
 
-core config:
-  dbname: $DB_NAME
-  dbuser: $DB_USER
-  dbpass: $DB_PASSWORD
-  dbhost: $DB_HOST:$DB_PORT
+  mkdir -p public/content/plugins
+  mkdir -p public/content/themes
 
-apache_modules:
-  - mod_rewrite
+  cat << EOF > public/index.php
+  <?php
+  define('WP_USE_THEMES', true);
+  require('wordpress/wp-blog-header.php');
+EOF
+}
 
-ssh:
-  vagrant:
-    # The %pseudotty% placeholder gets replaced with -t or -T depending on whether you're piping output
-    # The %cmd% placeholder is replaced with the originally-invoked WP-CLI command
-    cmd: vagrant ssh-config > /tmp/vagrant_ssh_config && ssh -q %pseudotty% -F /tmp/vagrant_ssh_config default %cmd%
-    # Passed to WP-CLI on the remote server via --url
-    url: 192.168.33.20
-    # We cd to this path on the remote server before running WP-CLI
-    path: /vagrant/sites/wp-starter/public/wordpress/
+function download_wordpress {
+  wp core download
+}
+
+function configure_wordpress {
+  echo "--- Configuring installation ---"
+
+  wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST:$DB_PORT
+
+  cat << EOF > public/wp-config.php
+  <?php
+
+  define('APP_ROOT', dirname(__DIR__));
+  // define('APP_ENV', getenv('APPLICATION_ENV'));
+  // define('APP_ENV', 'development');
+
+  define('WP_HOME', 'http://$MY_IP:$MY_PORT/$MY_SUBPATH$SITE_NAME/public');
+  define('WP_SITEURL', WP_HOME . '/wordpress/');
+
+  define('WP_CONTENT_DIR', APP_ROOT . '/public/content');
+  define('WP_CONTENT_URL', WP_HOME . '/content');
+
+  define('WP_DEBUG', false);
+
 EOF
 
-mkdir -p public/content/plugins
-mkdir -p public/content/themes
+  tail -n +2 public/wordpress/wp-config.php >> public/wp-config.php
 
-cat << EOF > public/index.php
-<?php
-define('WP_USE_THEMES', true);
-require('wordpress/wp-blog-header.php');
-EOF
+  rm public/wordpress/wp-config.php
+}
 
-wp core download
+function install_wordpress {
+  echo "--- Installing WordPress ---"
+  wp core install
+}
 
-echo "--- Configuring installation ---"
+function install_theme {
+  echo "--- Installing Theme ---"
+  cd public/content/themes
+  git clone git@github.com:baze/wp-starter-theme.git
+  wp theme activate $THEME_NAME
+}
 
-wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST:$DB_PORT
+function install_plugins {
+  echo "--- Installing Plugins ---"
+  wp plugin install 'timber-library' --activate
 
-cat << EOF > public/wp-config.php
-<?php
-
-define('APP_ROOT', dirname(__DIR__));
-// define('APP_ENV', getenv('APPLICATION_ENV'));
-// define('APP_ENV', 'development');
-
-define('WP_HOME', 'http://$MY_IP:$MY_PORT/$MY_SUBPATH$SITE_NAME/public');
-define('WP_SITEURL', WP_HOME . '/wordpress/');
-
-define('WP_CONTENT_DIR', APP_ROOT . '/public/content');
-define('WP_CONTENT_URL', WP_HOME . '/content');
-
-define('WP_DEBUG', false);
-
-EOF
-
-tail -n +2 public/wordpress/wp-config.php >> public/wp-config.php
-
-rm public/wordpress/wp-config.php
-
-
-
-echo "--- Installing WordPress ---"
-wp core install
-
-# wp user create starter-admin martensen@euw.de --user_pass=starter-password --role=administrator
-
-echo "--- Installing Theme ---"
-cd public/content/themes
-git clone git@github.com:baze/wp-starter-theme.git
-wp theme activate $THEME_NAME
-
-
-
-echo "--- Installing Plugins ---"
-wp plugin install 'timber-library' --activate
-
-for PLUGIN in 'advanced-custom-fields' 'contact-form-7' 'contact-form-7-honeypot' 'custom-post-type-ui' 'ewww-image-optimizer' 'redirection' 'regenerate-thumbnails' 'reveal-ids-for-wp-admin-25' 'stream' 'taxonomy-terms-order' 'wordpress-importer' 'wordpress-seo' 'wp-ban' 'wp-html-compression'
-do
+  for PLUGIN in 'advanced-custom-fields' 'contact-form-7' 'contact-form-7-honeypot' 'custom-post-type-ui' 'ewww-image-optimizer' 'redirection' 'regenerate-thumbnails' 'reveal-ids-for-wp-admin-25' 'stream' 'taxonomy-terms-order' 'wordpress-importer' 'wordpress-seo' 'wp-ban' 'wp-html-compression'
+  do
     wp plugin install $PLUGIN --activate
-done
+  done
 
-wp plugin install 'mappress-google-maps-for-wordpress'
+  wp plugin install 'mappress-google-maps-for-wordpress'
 
-wp plugin install 'polylang'
+  wp plugin install 'polylang'
+}
 
+function install_plugins_nonfree {
+  echo "--- Installing ACF non-free plugins ---"
 
-echo "--- Installing ACF non-free plugins ---"
+  pushd public/content/plugins/
 
-pushd public/content/plugins/
+  if [ ! -z "$ACF_REPEATER_KEY" ]; then
+    wget -O acf-repeater.zip http://download.advancedcustomfields.com/$ACF_REPEATER_KEY/trunk/ && unzip acf-repeater.zip && rm acf-repeater.zip
+    wp plugin activate 'acf-repeater'
+  fi
 
-if [ ! -z "$ACF_REPEATER_KEY" ]; then
-	wget -O acf-repeater.zip http://download.advancedcustomfields.com/$ACF_REPEATER_KEY/trunk/ && unzip acf-repeater.zip && rm acf-repeater.zip
-	wp plugin activate 'acf-repeater'
-fi
+  if [ ! -z "$ACF_OPTIONS_PAGE_KEY" ]; then
+    wget -O acf-options-page.zip http://download.advancedcustomfields.com/$ACF_OPTIONS_PAGE_KEY/trunk/ && unzip acf-options-page.zip && rm acf-options-page.zip
+    wp plugin activate 'acf-options-page'
+  fi
 
-if [ ! -z "$ACF_OPTIONS_PAGE_KEY" ]; then
-	wget -O acf-options-page.zip http://download.advancedcustomfields.com/$ACF_OPTIONS_PAGE_KEY/trunk/ && unzip acf-options-page.zip && rm acf-options-page.zip
-	wp plugin activate 'acf-options-page'
-fi
+  popd
+}
 
-popd
+function publish_content {
+  # Delete Sample Data
+  wp post delete 1 2 --force
 
+  echo "--- Creating Pages ---"
+  for PAGENAME in 'Startseite' 'Kontakt' 'Anfahrt' 'Impressum' 'Datenschutz'
+  do
+    wp post create --post_type=page --post_title=$PAGENAME  --post_status=publish
+  done
+}
 
+function update_permalinks {
+  wp rewrite structure '/%year%/%monthnum%/%day%/%postname%/' --hard
+  wp rewrite flush --hard
+}
 
-echo "--- Creating Pages ---"
-for PAGENAME in 'Startseite' 'Kontakt' 'Anfahrt' 'Impressum' 'Datenschutz'
-do
-	wp post create --post_type=page --post_title=$PAGENAME
-done
+function show_site {
+  URL=http:$MY_IP:$MY_PORT/$MY_SUBPATH$SITE_NAME/public
+  echo "--- All done! Visit $URL ---"
+  open $URL
+}
 
-wp rewrite structure '/%year%/%monthnum%/%day%/%postname%/' --hard
-
-wp rewrite flush --hard 
-
-URL=http:$MY_IP:$MY_PORT/$MY_SUBPATH$SITE_NAME/public
-echo "--- All done! Visit $URL ---"
-open $URL
+create_database
+configure_wp_cli
+download_wordpress
+configure_wordpress
+install_wordpress
+install_theme
+install_plugins
+install_plugins_nonfree
+publish_content
+update_permalinks
+show_site
